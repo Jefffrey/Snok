873d33a8fd35c62d38f7d12bc7df57c631a5a938
Jefffrey <notjefff@gmail.com>
2015-03-30 19:48:46 +0200

    Removed foreign function interface

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,50 @@
 module Main where
 
 import           Snok.Playable
-import           Snok.Shaders
 import           Graphics.Rendering.OpenGL (($=))
+import           Control.Applicative
+import           System.FilePath ((</>))
 import qualified Graphics.Rendering.OpenGL as GL
+import qualified Graphics.GLUtil as GLU
 
-newtype Game = Game ()
+shaderPath :: String
+shaderPath = "shaders/"
 
-instance Initializable Game where
-    initialize _ =
-        -- GL.clearColor $= GL.Color4 1 0 0 1 | gives segfault
-        return ()
+vertices :: [Float]
+vertices = [  0.0,  0.8
+           , -0.8, -0.8
+           ,  0.8, -0.8
+           ]
+
+data Program = Program GL.Program GL.AttribLocation GL.BufferObject
+
+initGame :: IO Game
+initGame = do
+    GL.blend $= GL.Enabled
+    GL.blendFunc $= (GL.SrcAlpha, GL.OneMinusSrcAlpha)
+    vs <- GLU.loadShader GL.VertexShader $ shaderPath </> "a.vs"
+    fs <- GLU.loadShader GL.FragmentShader $ shaderPath </> "a.fs"
+    p <- GLU.linkShaderProgram [vs, fs]
+    program <-
+        Program
+            <$> GLU.linkShaderProgram [vs, fs]
+            <*> GL.get (GL.attribLocation p "coord2d")
+            <*> GLU.makeBuffer GL.ArrayBuffer vertices
+    return $ Game program
+
+newtype Game = Game Program
 
 instance Drawable Game where
-    draw _ =
+    draw (Game (Program program attrib buf)) = do
+        GL.clearColor $= GL.Color4 1 1 1 1
         GL.clear [GL.ColorBuffer]
+        GL.currentProgram $= Just program
+        GL.vertexAttribArray attrib $= GL.Enabled
+        GL.bindBuffer GL.ArrayBuffer $= Just buf
+        GL.vertexAttribPointer attrib $=
+            (GL.ToFloat, GL.VertexArrayDescriptor 2 GL.Float 0 GLU.offset0)
+        GL.drawArrays GL.Triangles 0 3 -- 3 is the number of vertices
+        GL.vertexAttribArray attrib $= GL.Disabled
 
 main :: IO ()
-main = play (Game ())
+main = initGame >>= play
