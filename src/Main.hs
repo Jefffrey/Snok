01fe08c5c5ab5053b83761c9f5c0baaf48c7de81
Jefffrey <notjefff@gmail.com>
2015-03-30 18:07:53 +0200

    Working window opening

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,17 @@
 module Main where
 
-import Snok.Engine
+import           Snok.Engine
+import           Graphics.Rendering.OpenGL (($=))
+import qualified Graphics.Rendering.OpenGL as GL
+
+newtype Game = Game ()
+
+instance Initializable Game where
+    initialize _ = return ()
+
+instance Drawable Game where
+    draw _ =
+        GL.clear [GL.ColorBuffer]
 
 main :: IO ()
-main = do
-    let win = Window "Example" (640, 480)
-    withEngine $ withWindow win $
-        return ()
+main = play (Game ())
--- src/Snok/Engine.hs
+++ src/Snok/Engine.hs
@@ -1,9 +1,19 @@
-module Snok.Engine where
+module Snok.Engine
+    ( Initializable(..)
+    , Drawable(..)
+    , play ) where
 
 import qualified Graphics.UI.GLFW as GLFW
 import           System.Exit (exitSuccess, exitFailure)
-import           System.IO (stderr, hPutStrLn)
+import           System.IO (stderr, hPutStrLn, hFlush)
 import           Control.Monad (when, unless)
+import           Control.Concurrent.STM
+
+data Event
+    = KeyPressed GLFW.Key
+    | KeyReleased GLFW.Key
+    deriving (Eq)
+type EventQueue = TQueue Event
 
 withEngine :: IO () -> IO ()
 withEngine action = do
@@ -14,16 +24,21 @@ withEngine action = do
         action
         GLFW.terminate
         exitSuccess
-    else exitFailure
-    where errorCallback :: GLFW.ErrorCallback
-          errorCallback err = hPutStrLn stderr
+    else error "Woops, engine not initialized"
 
 data Window = Window String (Int, Int) deriving (Eq)
 
-keyCallback :: GLFW.Window -> GLFW.Key -> Int -> GLFW.KeyState -> GLFW.ModifierKeys -> IO ()
-keyCallback window key _ state _ =
-    when (key == GLFW.Key'Escape && state == GLFW.KeyState'Pressed) $
-        GLFW.setWindowShouldClose window True
+errorCallback :: GLFW.ErrorCallback
+errorCallback err msg = do
+    hPutStrLn stderr $ show err ++ msg
+    hFlush stderr
+
+keyCallback :: EventQueue -> GLFW.Window -> GLFW.Key -> Int -> GLFW.KeyState -> GLFW.ModifierKeys -> IO ()
+keyCallback events _ key _ state _ =
+    let event = (case state of
+            GLFW.KeyState'Pressed -> KeyPressed key
+            GLFW.KeyState'Released -> KeyReleased key)
+    in atomically $ writeTQueue events event
 
 windowLoop :: GLFW.Window -> IO () -> IO ()
 windowLoop window action = do
@@ -37,15 +52,29 @@ windowLoop window action = do
 withWindow :: Window -> IO () -> IO ()
 withWindow (Window title (w, h)) action = do
     GLFW.windowHint $ GLFW.WindowHint'OpenGLDebugContext True
-    GLFW.windowHint $ GLFW.WindowHint'DepthBits 16
-    win <- GLFW.createWindow w h title Nothing Nothing
+    GLFW.windowHint $ GLFW.WindowHint'ContextVersionMajor 3
+    GLFW.windowHint $ GLFW.WindowHint'ContextVersionMinor 3
+    GLFW.windowHint $ GLFW.WindowHint'OpenGLForwardCompat True
+    GLFW.windowHint $ GLFW.WindowHint'OpenGLProfile GLFW.OpenGLProfile'Core
+    events  <- (newTQueueIO :: IO EventQueue)
+    win     <- GLFW.createWindow w h title Nothing Nothing
     case win of
-        Nothing -> GLFW.terminate >> exitFailure
+        Nothing     -> GLFW.terminate >> error "Could not init window"
         Just window -> do
-            GLFW.makeContextCurrent (Just window)
-            GLFW.setKeyCallback window (Just keyCallback)
+            GLFW.makeContextCurrent $ Just window
+            GLFW.setKeyCallback window $ Just $ keyCallback events
             windowLoop window action
             GLFW.destroyWindow window
 
+class Initializable t where
+    initialize :: t -> IO ()
+
 class Drawable t where
-    render :: t -> IO ()
+    draw :: t -> IO ()
+
+play :: (Initializable t, Drawable t) => t -> IO ()
+play obj = 
+    withEngine $ do
+        let win = Window "Example" (640, 480)
+        initialize obj
+        withWindow win $ draw obj
